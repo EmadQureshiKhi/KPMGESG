34ad9f831ce37858e02b2c64f3ba70dbaf001f68
NOKITHA <nokitha@NOKITHAs-MacBook-Pro.local>
2025-07-01 14:49:56 +0500

    api fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = 'https://esgbackend-hqvt.onrender.com';
 console.log('API_BASE_URL:', API_BASE_URL);
 
 const api = axios.create({
